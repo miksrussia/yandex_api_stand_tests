fd0cd57406bd7c03d059dfc53108554a9bc8b093
miksrussia <153459866+miksrussia@users.noreply.github.com>
2023-12-12 21:04:24 +0300

    Add files via upload

--- configuration.py
+++ configuration.py
@@ -1,3 +1,5 @@
-URL_SERVICE =  "https://ea1aca09-d6aa-4bb9-a84f-7781adee9284.serverhub.praktikum-services.ru"
+URL_SERVICE =  "https://46c7d977-08a6-47d2-92b6-d92c1da3938a.serverhub.praktikum-services.ru"
+
+
 CREAT_ORDERS = "/api/v1/orders"
 ORDER = "/api/v1/orders/track?t="
--- sendor_stand_request.py
+++ sendor_stand_request.py
@@ -7,10 +7,12 @@ def post_new_order(body):
                          json=body,
                          headers=data.headers)
 
-data.order_track = str(post_new_order(data.order_body).json()['track'])
+# data.order_track = str(post_new_order(data.order_body).json()['track'])
 
 def get_order_by_number(order_track):
     return requests.get(configuration.URL_SERVICE + configuration.ORDER + order_track)
 
-response = get_order_by_number(data.order_track)
-print(response.json())
+
+# response = post_new_order(data.order_body)
+# print(response)
+# print(response.json())
